c97b7ea3abdfda2f793ba575c600728962ef65e2
Niharrika <59138295+Niharrika@users.noreply.github.com>
2020-03-06 13:04:59 +0530

    Update 1.js

--- IIFE/1.js
+++ IIFE/1.js
@@ -1,15 +1,14 @@
-(function () {
+(function() { 
 b=[];
- for (i=2;i<=l.length;i++){
-	 var a = true;
-	 for(j=2;j<i;j++){
-		 if(i%j==0 && i!=j){
-			 var a = false;
-		 }
-	 }
-	 if(a == true){
-		 b.push(i);
-	 }
- }
- console.log(b);
-})(l=[1,2,3,4,5,6,7,8,9,10,11,12]);
+c=[];
+
+for(i=0;i<a.length;i++){
+	if(i%2==0){
+	b.push(i);
+	}
+	else{
+	c.push(i);
+	}
+}
+console.log(c);
+})(a=[1,2,3,4,5,6,7,8,9,10,11,12,13]);
